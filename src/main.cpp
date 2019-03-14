85f9133b107d9d6a4afcf4f69549b8fb3710468a
cpiemontese <cristiano.piemontese@gmail.com>
2019-03-15 00:13:28 +0100

    Refactored solution data representation

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,33 @@
 #include <fstream>
 #include <iostream>
-#include <memory>
+//#include <memory>
+#include <vector>
 using namespace std;
 
 struct Photo {
-    string kind;
+    int id;
     int tag_num;
     string** tags;
 };
+
+enum struct SlideKind { H, V };
+
+struct Slide {
+    SlideKind kind;
+    union {
+        Photo horizontal_photo;                 // SlideKind::H
+        pair<Photo, Photo> vertical_photos;     // SlideKind::V
+    };
+};
+
+int score(Slide s1, Slide s2) {
+    // trova tag in comune
+    // calcola # tag in comune
+    // calcola # tag in s1 e NON in s2 i.e. tag s1 != tag comune
+    // calcola # tag in s2 e NON in s1 i.e. tag s2 != tag comune
+    int score = 0;
+    return score;
+}
 
 int main(int argc, char** argv) {
     if (argc != 3) {
@@ -19,15 +39,41 @@ int main(int argc, char** argv) {
     int lines;
     infile >> lines;
 
-    Photo photos[lines];
+    // foto orizzontali -> gia' delle slides
+    // foto verticali -> le separo, faccio pre-processing
+    // dopo il pre-processing delle verticali creo un array t.c. id della slide
+    // e' la posizione occupata nell'array, in modo da poter shufflare in
+    // maniera banale per creare nuove soluzioni, i.e. lavoro sugli indici
+
+    Slide slides[lines]; // valore finale dopo preproc verticali
+    vector<Photo> verticals;
+
+    string kind;
+    Photo current_photo;
+    int current_slide_id = 0;
     for (int l = 0; l < lines; l++) {
-        infile >> photos[l].kind >> photos[l].tag_num;
-        photos[l].tags = new string*[photos[l].tag_num];
-        for (int t = 0; t < photos[l].tag_num; t++) {
-            photos[l].tags[t] = new string;
-            infile >> *photos[l].tags[t];
+        infile >> kind;
+        infile >> current_photo.tag_num;
+
+        current_photo.id = l;
+        current_photo.tags = new string*[current_photo.tag_num];
+        for (int t = 0; t < current_photo.tag_num; t++) {
+            current_photo.tags[t] = new string;
+            infile >> *current_photo.tags[t];
         }
+
+        if (kind == "H") {
+            slides[current_slide_id].kind = SlideKind::H;
+            slides[current_slide_id].horizontal_photo = current_photo; // ptr?
+            current_slide_id++;
+        } else
+            verticals.push_back(current_photo); // ptr?
     }
+
+    // vertical_slides = local_search_verticals(verticals)
+    // aggiungi vertical_slides a slides
+    // slides = local_search_whole(slides)
+    // produci out scorrendo le slides i.e. come sono ordinate e' l'output
 
     return 0;
 }
